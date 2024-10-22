737e9863cece11de3e408fb034129dc8803cd61f
darlyganza <umumararunguganzadarlene@gmail.com>
2024-10-22 16:53:14 +0200

    Arrays data described in details

---
+++ src/Basics/Arrays.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Arrays {
+	public static void main(String[] args) {
+		//creation of arrays in java use the [] after the datatype then the array name
+		int[] arrValues = new int[8];
+		arrValues[0] = 12;
+		arrValues[1]= 21;
+		arrValues[2]= 34;
+		arrValues[3]= 11;
+		arrValues[4]= 52;
+		arrValues[5]=11;
+		arrValues[6]= 19;
+		arrValues[7]= 26;
+	System.out.println(arrValues[3]);
+	}
+}
