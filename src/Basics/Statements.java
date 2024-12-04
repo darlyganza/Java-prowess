71b91c0dafe05959046d50525f06c7c9f58cd252
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-04 20:00:28 +0100

    Added statements.java

---
+++ src/Basics/Statements.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Statements {
+	//showcasing statements and their abilities to : declare variables, assign values to variables, printing text to the console among others;
+	public static void main(String[] args) {
+		int number =12; //variable initialisation 
+		System.out.println("This statement outputs text to the console and prints varibale"+ number);
+	}
+
+}
