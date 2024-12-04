0af1c424c28e2b258bd018a2447bf7eb17454cf4
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-04 20:04:33 +0100

    --allow-empty-message

---
+++ src/Basics/Expressions.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Expressions {
+	public static void main(String[] args) {
+		int number1 = 12;
+		int number2= 123;
+		System.out.println(number1+number2+" An expression that adds two numbers can also be done at the initialisation stage");
+		String myName = "GANZA"+" Darlene";
+		System.out.println("My name is: "+ myName);
+	}
+
+}
--- src/Basics/Statements.java
+++ src/Basics/Statements.java
@@ -4,7 +4,7 @@ public class Statements {
 	//showcasing statements and their abilities to : declare variables, assign values to variables, printing text to the console among others;
 	public static void main(String[] args) {
 		int number =12; //variable initialisation 
-		System.out.println("This statement outputs text to the console and prints varibale"+ number);
+		System.out.println("This statement outputs text to the console and prints varible "+ number);
 	}
 
 }
