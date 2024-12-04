5900ca8f591243a2c09e3176a3d5eef177a34ea6
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-04 20:07:52 +0100

    Showcased the various comments a  java application is likely to have

---
+++ src/Basics/Comments.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Java commnents are various like the javadoc /** */ comments and the multi line comments /* */ upto the single line comments //");
+	}
+
+}
