e8ac4d6bf9b0dcf3d0086e76d318d290675f3f2f
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-05 21:18:56 +0100

    Described arrays and how to access them element by element

---
+++ src/Basics/Arrays.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Arrays {
+	public static void main(String[] args) {
+		//java arrays are created and the size is generated and fixed after the array creation: Arrays can be accessed using the array indices 
+		//array indices start from 0 and on to the last element in the array:
+		
+		int[] numbers = {1,2,3,4,45,5,66,9}; //curly braces used to show arrays
+		System.out.println(numbers[3]);
+	}
+
+}
