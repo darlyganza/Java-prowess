e50a8938ecfcfa0607cf7a3aed54c3f2869f2c1f
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-05 21:42:36 +0100

    --allow-empty message

--- src/Basics/Arrays.java
+++ src/Basics/Arrays.java
@@ -1,7 +1,9 @@
 package Basics;
-
+import java.util.Scanner;
 public class Arrays {
 	public static void main(String[] args) {
+		Scanner myScan = new Scanner(System.in);
+		
 		//java arrays are created and the size is generated and fixed after the array creation: Arrays can be accessed using the array indices 
 		//array indices start from 0 and on to the last element in the array:
 		
@@ -9,8 +11,8 @@ public class Arrays {
 		System.out.println(numbers[3]);
 		
 		for (int i=1;i<numbers.length;i++) {
-			System.out.println(numbers[i]);
-		}
+			System.out.println(numbers[i]);//looping through the elements of the array using the for loop
+		}	
 	}
 
 }
---
+++ src/Basics/Object.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class Object {
+	public static void main(String[] args) {
+		
+		//objects: Complex data types in java
+		//objects: collection of codes that resemble real word objects and can shwo state and behaviour:
+		
+	String myString = new String ("Hello world, Welcome to objects");
+	//String the original class where the object is derived, myString: The variable reference and they are created using the new keyword
+	System.out.println(myString);
+	myString = null;
+	
+	//this means that myString object referencing varible will now be empty (i.e null);
+	
+	System.out.println(myString);
+	}
+}
