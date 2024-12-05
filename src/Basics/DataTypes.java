07837e45623cf715eda25902fcefd9ec2b8610f0
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-05 21:12:30 +0100

    --allow-unrelated-message

--- src/Basics/DataTypes.java
+++ src/Basics/DataTypes.java
@@ -1,5 +1,21 @@
 package Basics;
 
 public class DataTypes {
+	public static void main(String[] args) {
+		//java data types are primitive and complex ones:
+		//primitive: 
+		int age = 12;
+		System.out.println(age);
+		
+		//showcasing casting: 
+		
+		long number = 123456789;
+		int num1 = (int)number;
+		short num2 = (short)number;
+		System.out.println(num2);
+		
+		
+		
+	}
 
 }
--- src/Basics/Variables.java
+++ src/Basics/Variables.java
@@ -1,5 +1,11 @@
 package Basics;
 
 public class Variables {
+	public static void main(String[] args) {
+		//java variables: The variables in java programming language range from primitive to complex types:
+		int number = 12;
+		System.out.println(number+ " is a number value stored in the number variable declared with the int as a data type"); 
+		//data types will help to prepare the memory for the data it is to keep
+	}
 
 }
